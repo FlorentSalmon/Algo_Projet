09e3e1c60d37e17b6d785e9e353bdbfece18496f
Labyrinthegreg <leo.gregori95@gmail.com>
2021-01-27 14:51:33 +0100

    Creer d'un fichier.json ave les films et leurs années de sorti

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 fs = require('fs')
 
-fs.readFile('Json/movies2.json',{encoding: 'utf8'},function(err,data) {
+fs.readFile('Json/movies.json',{encoding: 'utf8'},function(err,data) {
     if(err) return console.error(err);
     movies = JSON.parse(data);
     for(i = 0; i < movies.length; i++){
         let index = movies[i];
         let date = new Date(index['release_date'] * 1000);
         let year = date.getFullYear();
-        let title = index['title'] + '( ' + year + ' )';
+        let title = index['title'] + ' (' + year + ') ';
         index['title'] = title;
     }
-    console.log(movies);
+    for(i in movies){
+        if(movies[i] == "}" ){
+            movies[i] = "},\n" ;
+            movies[i+1] = ""; 
+        }
+    }
+    movies = JSON.stringify(movies);
+
+    fs.writeFile('Json/moviesDate.json',movies,function(err) {
+        if(err) return console.error(err);
+        console.log('done');
+        })
 })
+
+
+
