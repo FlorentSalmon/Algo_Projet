ee86d5061bd1574f180ab81b71bb205dee6697a6
Labyrinthegreg <leo.gregori95@gmail.com>
2021-01-27 16:42:35 +0100

    Menu help pour les arguments

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
 fs = require('fs')
 
-fs.readFile('Json/movies.json',{encoding: 'utf8'},function(err,data) {
-    if(err) return console.error(err);
-    movies = JSON.parse(data);
-    for(i = 0; i < movies.length; i++){
-        let index = movies[i];
-        let date = new Date(index['release_date'] * 1000);
-        let year = date.getFullYear();
-        let title = index['title'] + ' (' + year + ') ';
-        index['title'] = title;
-    }
-    movies = JSON.stringify(movies, null, 3);
-
-    fs.writeFile('Json/moviesDate.json',movies,function(err) {
+function read_and_write_file(fileIn, fileOut){
+    fs.readFile(fileIn,{encoding: 'utf8'},function(err,data) {
         if(err) return console.error(err);
-        console.log('done');
-        })
-})
+        movies = JSON.parse(data);
+        for(i = 0; i < movies.length; i++){
+            let index = movies[i];
+            let date = new Date(index['release_date'] * 1000);
+            let year = date.getFullYear();
+            let title = index['title'] + ' (' + year + ')';
+            index['title'] = title;
+        }
+        movies = JSON.stringify(movies, null, 3);
 
-<<<<<<< HEAD
-=======
-let start = new Date().getTime();
-    
-let stop = new Date().getTime();
-console.log("time : " + (stop - start));
+        fs.writeFile(fileOut,movies,function(err) {
+            if(err) return console.error(err);
+            console.log('done');
+            })
+    })
+}
+
+//read_and_write_file('Json/movies.json', 'Json/moviesDate.json');
 
 function sort_date(){
-    console.log("Danse la macarena")
+    console.log('Winner')
 }
 
-console.log(process.argv);
-if(process.argv[2] == '-lol'){
-    sort_date();
+function sort_titre(){
+    console.log('Looser')
 }
->>>>>>> main
+
+arg = process.argv;
+for(i = 0; i < arg.length; i++){
+    if(arg[i] == '-action'){
+        if(arg[i+1] == 'sort_date'){
+            sort_date();
+        }
+        if(arg[i+1] == 'sort_titre'){
+            sort_titre();
+        }
+    }
+    if(arg[i] == "--h" || arg[i] == "--help"){
+        console.log("Voici une aide à ce que peut faire le programme: \n" + 
+        "\t --help: Résumé de toutes les actions possibles \n" + 
+        "\t -action: Permet de réaliser des actions: \n" + 
+        "\t \t -sort_date: sort_date ./fichierEntrer.json ./fichierSortie.json ; Pour oragniser votre fichjier dans l'ordre annuel \n" + 
+        "\t \t -sort_titre: sort_titre ./fichierEntrer.json ./fichierSortie.json ; Pour oragniser votre fichjier dans l'ordre alphabetique\n" +
+        "\t \t -search_date: search_date ./fichierEntrer.json <year> <sorted>(true or false) ; Pour chercher tous les films d'une certaine année \n" +
+        "\t \t -search_key_word: search_key_word ./fichierEntrer.json <key_word> <genre> ; Pour chercher tous les films d'un genre donnée avec un mot-clé présent dans sa description'\n" +
+        "\t \t -color <CheminDossier> ; Pour connaitre la couleur moyenne des affiches de films \n");
+    }
+}
